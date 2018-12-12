b0c810cbd640c1b628c6dddba080ea99bef51f34
Sabrina Jiang <sabrinajiang@Sabrinas-MacBook-Pro.local>
2018-12-12 16:29:07 -0600

    Prepped for timing ctr_parallel

--- gen.py
+++ gen.py
@@ -2,7 +2,51 @@ import random
 import string
 from random import randint
 
-blocksizes = {1, 10, 100, 1000, 2000, 5000, 10000}
+
+blocksizes = {1, 10, 100, 1000, 10000}
+inputsizes = {10000}
+
+for inputsize in inputsizes:
+        for blocksize in blocksizes:
+                s = ""
+                s += str(blocksize)
+                s += "\n"
+                for i in range(0, inputsize):
+                        s += str(randint(0, 1))
+                s += "\n"
+                f = open("ctr_input_" + str(inputsize) + "_" + str(blocksize) + ".txt", "x")
+                f.write(s)
+
+blocksizes = {1, 10, 100, 1000, 10000, 100000}
+inputsizes = {100000}
+
+for inputsize in inputsizes:
+        for blocksize in blocksizes:
+                s = ""
+                s += str(blocksize)
+                s += "\n"
+                for i in range(0, inputsize):
+                        s += str(randint(0, 1))
+                s += "\n"
+                f = open("ctr_input_" + str(inputsize) + "_" + str(blocksize) + ".txt", "x")
+                f.write(s)
+
+blocksizes = {1, 10, 100, 1000, 10000, 100000, 1000000}
+inputsizes = {1000000}
+
+for inputsize in inputsizes:
+        for blocksize in blocksizes:
+                s = ""
+                s += str(blocksize)
+                s += "\n"
+                for i in range(0, inputsize):
+                        s += str(randint(0, 1))
+                s += "\n"
+                f = open("ctr_input_" + str(inputsize) + "_" + str(blocksize) + ".txt", "x")
+                f.write(s)
+
+
+blocksizes = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000}
 inputsizes = {10000000}
 
 for inputsize in inputsizes:
